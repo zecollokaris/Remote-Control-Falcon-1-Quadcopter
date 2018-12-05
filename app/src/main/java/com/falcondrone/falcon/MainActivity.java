eb9a982a870e1b013e7c6fa8a53a4020106ee682
Wess58 <wesleymbate@gmail.com>
2018-12-05 18:25:53 +0300

    Bind views necessary for controls

--- app/src/main/java/com/falcondrone/falcon/MainActivity.java
+++ app/src/main/java/com/falcondrone/falcon/MainActivity.java
@@ -2,12 +2,32 @@ package com.falcondrone.falcon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.upDirection) CircleImageView mUpDirection;
+    @BindView(R.id.downDirection) CircleImageView mDownDirection;
+    @BindView(R.id.leftDirection) CircleImageView mLeftDirection;
+    @BindView(R.id.rightDirection) CircleImageView mRightDirection;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
