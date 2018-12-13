b6e100089a7246bc942e33ce13ef39db140fa622
Wess58 <wesleymbate@gmail.com>
2018-12-13 19:50:15 +0300

    binded views in main.xml

--- app/src/main/java/com/falcondrone/falcon/MainActivity.java
+++ app/src/main/java/com/falcondrone/falcon/MainActivity.java
@@ -2,18 +2,21 @@ package com.falcondrone.falcon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.View;
+
+import com.ramotion.fluidslider.FluidSlider;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnDragListener{
 
     @BindView(R.id.directionJoyStick) JoystickView mDirectionJoyStick;
+    @BindView(R.id.fluidslider) FluidSlider mFluidslider;
 
-
-    @Override
+       @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -23,7 +26,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v) {
+    public  void onBackPressed(){
+        moveTaskToBack(false);
+    }
 
+
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        return false;
     }
 }
